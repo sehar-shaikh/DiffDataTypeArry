b8fa0a75ae09078543ff522c4e556a9e937aa6fe
Sehar <Sehar@DESKTOP-F5C6FMG>
2023-03-28 20:23:39 +0500

    Add project files.

---
+++ DiffDataTypeArry/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DiffDataTypeArry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] arr = new object[4];
+            arr[0] = 1;
+            arr[1] = "sehar";
+            arr[2] = 5.2;
+            arr[3] = true;
+          
+
+            foreach (object item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
